ee9b28f6540b465b6536f66c6ced7ca9117633f6
unknown <quocmrh3llo@gmail.com>
2026-06-08 16:15:26 +0700

    Added try-catch to display message error to user

--- product/UngDungQuanLyHoSoSinhVien/KetNoiCSDL.cs
+++ product/UngDungQuanLyHoSoSinhVien/KetNoiCSDL.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-
 using System.Data;
 using System.Data.SqlClient;
+using System.Text;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
 
 namespace UngDungQuanLyHoSoSinhVien
 {
@@ -24,34 +24,58 @@ namespace UngDungQuanLyHoSoSinhVien
 
         public DataTable GhiDuLieuVaoBang(string SQL_TruyVan)
         {
-            SQL_KetNoi.Open();
-            SQL_DocGhi = new SqlDataAdapter(SQL_TruyVan, SQL_KetNoi);
-            DataTable Bang = new DataTable();
-            SQL_DocGhi.Fill(Bang);
-            SQL_KetNoi.Close();
-            return Bang;
+            try
+            {
+                SQL_KetNoi.Open();
+
+                SQL_DocGhi = new SqlDataAdapter(SQL_TruyVan, SQL_KetNoi);
+                DataTable Bang = new DataTable();
+                SQL_DocGhi.Fill(Bang);
+                SQL_KetNoi.Close();
+                return Bang;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi đọc dữ liệu: " + ex.Message, "Lỗi");
+                return new DataTable();
+            }
         }
 
         public void ThaoTac_Ghi_DuLieu(string SQL_TruyVan)
         {
-            SQL_KetNoi.Open();
-            SQL_Lenh = new SqlCommand(SQL_TruyVan, SQL_KetNoi);
-            SQL_Lenh.ExecuteNonQuery();
-            SQL_KetNoi.Close();
+            try
+            {
+                SQL_KetNoi.Open();
+
+                SQL_Lenh = new SqlCommand(SQL_TruyVan, SQL_KetNoi);
+                SQL_Lenh.ExecuteNonQuery();
+                SQL_KetNoi.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi đọc dữ liệu: " + ex.Message, "Lỗi");
+            }
         }
 
         public DataRow ThaoTac_DocMotDong_DuLieu(string SQL_TruyVan)
         {
             DataTable Bang = new DataTable();
-            
-            SQL_KetNoi.Open();
 
-            SqlCommand SQL_Lenh = new SqlCommand(SQL_TruyVan, SQL_KetNoi);
-            SqlDataAdapter SQL_DocGhi = new SqlDataAdapter(SQL_Lenh);
+            try
+            {
+                SQL_KetNoi.Open();
 
-            SQL_DocGhi.Fill(Bang);
+                SqlCommand SQL_Lenh = new SqlCommand(SQL_TruyVan, SQL_KetNoi);
+                SqlDataAdapter SQL_DocGhi = new SqlDataAdapter(SQL_Lenh);
 
-            SQL_KetNoi.Close();
+                SQL_DocGhi.Fill(Bang);
+
+                SQL_KetNoi.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Lỗi khi đọc dữ liệu: " + ex.Message, "Lỗi");
+            }
 
             return Bang.Rows.Count > 0 ? Bang.Rows[0] : null;
         }
